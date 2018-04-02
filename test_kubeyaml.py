9929f15d55d584057357e243601c7583fb0a547d
Michael Bridgen <mikeb@squaremobius.net>
2018-04-02 22:01:21 +0100

    Test update_image via roundtrip, with comments

--- test_kubeyaml.py
+++ test_kubeyaml.py
@@ -1,11 +1,16 @@
 import kubeyaml
 
 from hypothesis import given, assume, strategies as strats
+from hypothesis import reproduce_failure
 from hypothesis.strategies import composite
 from ruamel.yaml.compat import StringIO
+import string
 
 def strip(s):
     return s.strip()
+
+# I only want things that will got on one line, so make my own printable alphabet
+printable = string.ascii_letters + string.digits + string.punctuation + ' '
 
 # Names of things in Kubernetes are generally DNS labels.
 # https://github.com/kubernetes/community/blob/master/contributors/design-proposals/architecture/identifiers.md
@@ -25,15 +30,32 @@ hostnames_with_port = strats.builds(
     lambda h, p: str(h, ':', p),
     hostnames_without_port, port_numbers)
 
-hostnames = strats.one_of(hostnames_without_port, hostnames_with_port)
+hostnames = hostnames_without_port | hostnames_with_port
 
 alphanumerics = strats.characters(min_codepoint=48, max_codepoint=122,
                                   whitelist_categories=['Ll', 'Lu', 'Nd'])
-image_separators = strats.one_of(
-    strats.just('.'), strats.just('_'), strats.just('__'),
-    strats.integers(min_value=1, max_value=5).map(lambda n: '-' * n))
+image_separators = strats.just('.')  | \
+                   strats.just('_')  | \
+                   strats.just('__') | \
+                   strats.integers(min_value=1, max_value=5).map(lambda n: '-' * n)
 
 image_tags = strats.from_regex(r"^[\w][\w.-]{0,127}$").map(strip)
+
+@composite
+def commented(draw, values):
+    from ruamel.yaml.comments import CommentedMap
+    comments = strats.none() | strats.text(printable, min_size=1)
+
+    val = draw(values)
+    if type(val) is dict:
+        commentedval = CommentedMap()
+        i = 0
+        for k in val.keys():
+            comment = draw(comments)
+            commentedval.insert(i, k, val[k], comment=comment)
+            i = i + 1
+        val = commentedval
+    return val
 
 @composite
 def image_components(draw):
@@ -57,8 +79,10 @@ images_with_tag = strats.builds(
 # Kubernetes manifests
 # https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.9/
 
-kinds = strats.sampled_from(['Deployment', 'DaemonSet', 'StatefulSet', 'CronJob'])
-namespaces = strats.one_of(strats.just(''), dns_labels)
+controller_kinds = ['Deployment', 'DaemonSet', 'StatefulSet', 'CronJob']
+
+kinds = strats.sampled_from(controller_kinds)
+namespaces = strats.just('') | dns_labels
 
 def container(name, image):
     # TODO(michael): more to go here
@@ -86,7 +110,14 @@ def containers_equal(cs1, cs2):
         pass
     return False
 
-manifest_ids = strats.builds(lambda k, ns, n: (k, ns, n), kinds, dns_labels, namespaces)
+def manifest_id(man):
+    """These three values identify a resource uniquely"""
+    kind = man['kind']
+    ns = man['metadata'].get('namespace', 'default')
+    name = man['metadata']['name']
+    return kind, ns, name
+
+manifest_ids = strats.tuples(kinds, dns_labels, namespaces)
 
 @composite
 def manifests(draw):
@@ -96,7 +127,7 @@ def manifests(draw):
     if namespace != '':
         metadata['namespace'] = namespace
 
-    container_names = draw(strats.sets(max_size=5, average_size=2, elements=dns_labels))
+    container_names = draw(strats.sets(min_size=1, max_size=5, elements=dns_labels))
     containers = list(map(lambda n: container(n, draw(image_names)), container_names))
     podtemplate = {'template': {'spec': {'containers': containers}}}
 
@@ -123,10 +154,8 @@ class Spec:
 
     @staticmethod
     def from_manifest(man):
-        return Spec(kind=man['kind'],
-                    # The namespace is always given in the spec
-                    namespace=man['metadata'].get('namespace', 'default'),
-                    name=man['metadata']['name'])
+        (kind, ns, name) = manifest_id(man)
+        return Spec(kind=kind, namespace=ns, name=name)
 
     def __repr__(self):
         return "Spec(kind=%s,name=%s,namespace=%s)" % (self.kind, self.name, self.namespace)
@@ -166,7 +195,7 @@ def test_image_update(man, image, data):
         assert(outcs[ind]['image'] == image)
     assert(found)
 
-@given(strats.lists(elements=manifests(), max_size=5))
+@given(strats.lists(elements=commented(manifests()), max_size=5))
 def test_ident_apply(mans):
     yaml = kubeyaml.yaml()
     original = StringIO()
@@ -181,3 +210,41 @@ def test_ident_apply(mans):
 
     kubeyaml.apply_to_yaml(ident, infile, outfile)
     assert original.getvalue() == outfile.getvalue()
+
+@given(strats.lists(elements=commented(manifests()), min_size=1, max_size=5), strats.data())
+def test_update_image_apply(mans, data):
+    assume(len(mans) == len(set(map(manifest_id, mans))))
+
+    ind = data.draw(strats.integers(min_value=0, max_value=len(mans)-1))
+
+    yaml = kubeyaml.yaml()
+    original = StringIO()
+    for man in mans:
+        yaml.dump(man, original)
+    originalstr = original.getvalue()
+
+    man = mans[ind]
+    containers = kubeyaml.containers(man)
+    indc = data.draw(strats.integers(min_value=0, max_value=len(containers)-1))
+    spec = Spec.from_manifest(man)
+    spec.container = containers[indc]['name']
+    spec.image = data.draw(image_names)
+
+    infile, outfile = StringIO(originalstr), StringIO()
+    kubeyaml.apply_to_yaml(lambda ds: kubeyaml.update_image(spec, ds), infile, outfile)
+
+    updated = list(yaml.load_all(outfile.getvalue()))
+
+    # the selected manifest->container has the updated image; the rest
+    # are unchanged
+    assert len(updated) == len(mans)
+    found = False
+    for i in range(len(mans)):
+        if kubeyaml.match_manifest(spec, updated[i]):
+            found = True
+            c = kubeyaml.find_container(spec, updated[i])
+            assert c is not None
+            assert c['image'] == spec.image
+        else:
+            assert manifests_equal(mans[i], updated[i])
+    assert found
